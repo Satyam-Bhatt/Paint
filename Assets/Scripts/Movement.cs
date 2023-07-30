c25e86ac60ed7e99b470c631993b4f56706ceaf9
Satyam-Bhatt <satyambhatt20@gmail.com>
2023-07-30 20:45:26 +0530

    Rotation Added

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] private float speed = 100f;
+    [SerializeField] 
+    private float speed = 100f;
+    [SerializeField]
+    private float rotationSpeed = 1f;
 
     private Rigidbody2D rb;
     private Vector2 input;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
@@ -25,5 +29,14 @@ public class Movement : MonoBehaviour
         Vector2 movement = input * speed * Time.deltaTime;
         Vector2 newPosition = rb.position + movement;
         rb.MovePosition(newPosition);
+    }
+
+    private void Rotation()
+    {
+        if (input.magnitude > 0.5f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(transform.forward, input);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
     }
 }
