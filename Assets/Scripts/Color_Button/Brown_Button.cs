8c5073434787a303d93ced355c5ffe0e1ce837aa
Satyam-Bhatt <satyambhatt20@gmail.com>
2023-08-06 16:49:45 +0530

    Button Enable Disable

--- Assets/Scripts/Color_Button/Brown_Button.cs
+++ Assets/Scripts/Color_Button/Brown_Button.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Brown_Button : MonoBehaviour
 {
+    [SerializeField] private GameObject brownButton;
+
+    private void Start()
+    {
+        brownButton.SetActive(false);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
             Debug.Log("Player Hit");
+            brownButton.SetActive(true);
             Destroy(this.gameObject);
         }
     }
